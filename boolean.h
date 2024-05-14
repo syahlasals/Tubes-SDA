df5d50c7e66618078bc77b933bf7bebf2d04a6d2
syahlasals <syahlasbl28@gmail.com>
2024-05-14 10:10:24 +0700

    Menambahkan header boolean

---
+++ boolean.h
@@ -0,0 +1,13 @@
+/* File        : boolean.h 									*/  
+/* Deskripsi   : Header file untuk menangani type boolean	*/ 
+/* Dibuat oleh : Agra Anisa Ibtisamah		231511067
+				 Elsa Monika Sinaga     	231511074
+				 Syahla Salsabila			231511095 		*/
+/* Tanggal     : 14-05-2024 								*/ 
+
+#ifndef boolean_H
+#define boolean_H
+#define true 1
+#define false 0
+#define boolean unsigned char
+#endif
